b3e1c1850e38f44b187692f605bbca88376934f5
younseokChoi <cafebom@gmail.com>
2017-12-03 00:51:51 +0900

    추가.
    1. 포트폴리오 페이지 my-work부분 컨텐츠 추가
    
    2. 쓰리고 리뉴얼 푸터 부분 수정. 메뉴 사진에 호버시, 가격이 보여지게 추가함.

--- js/script.js
+++ js/script.js
@@ -1,51 +1,51 @@
 //nav-fixed-and color changing
 
-$(document).ready(function() {
-            $(window).scroll(function() {
-                var scroll = $(window).scrollTop();
-                if (scroll > 800) {
-                    $(".change-logo").css("display", "none");
-                    $(".change-logo-1").css("display", "block");
-                    $(".nav-text").css("color", "gray");
-                } else {
-                    $(".change-logo").css("display", "block");
-                    $(".change-logo-1").css("display", "none");
-                    $(".nav-text").css("color", "white");
-                }
-            });
-        });
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 800) {
+            $(".change-logo").css("display", "none");
+            $(".change-logo-1").css("display", "block");
+            $(".nav-text").css("color", "gray");
+        } else {
+            $(".change-logo").css("display", "block");
+            $(".change-logo-1").css("display", "none");
+            $(".nav-text").css("color", "white");
+        }
+    });
+});
 //nav-fixed-end
 
-        //text-animating
-        $(document).ready(function() {
-            $(window).scroll(function() {
-                var scroll = $(window).scrollTop();
-                if (scroll > 800) {
-                    $(".display-none").stop().animate({
-                        opacity: '1',
-                        top: '0'
-                    }, 300);
-                } else {
-                    $(".display-none").stop().animate({
-                        opacity: '0',
-                        top: '10'
-                    }, 300);
-                }
-            });
-        });
+//text-animating
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 800) {
+            $(".display-none").stop().animate({
+                opacity: '1',
+                top: '0'
+            }, 300);
+        } else {
+            $(".display-none").stop().animate({
+                opacity: '0',
+                top: '10'
+            }, 300);
+        }
+    });
+});
 //text-animation-end
 
-        //more-menu
+//more-menu
 
-        $(document).ready(function() {
-            $("#more-btn").click(function() {
-                $(".second-more-wrap-menu").slideToggle("slow");
-            })
-        });
+$(document).ready(function () {
+    $("#more-btn").click(function () {
+        $(".second-more-wrap-menu").slideToggle("slow");
+    })
+});
 //more-menu end
 
 
-        // tab-menu
+// tab-menu
 $(function () {
 
     $(".tab_content").hide();
@@ -60,3 +60,14 @@ $(function () {
         $("#" + activeTab).fadeIn()
     });
 });
+
+// tab-menu-end
+
+//more-menu
+
+$(document).ready(function () {
+    $("#more-btn-festival").click(function () {
+        $(".fifth-wrap-more").slideToggle("slow");
+    })
+});
+//more-menu end
